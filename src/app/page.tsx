6639fa4373df559d40fc3a73fa45463380de0d72
Ana Lima <af.rl@hotmail.com>
2023-09-04 20:27:02 -0300

    add: form settings and tabs components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,184 @@
+import { Input } from '@/components/Input'
+import { SettingsTabs } from '@/components/SettingsTabs'
+import { Mail, UploadCloud, User } from 'lucide-react'
+
 export default function Home() {
-  return <p className="text-clip">Page</p>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+
+      <div className="flex flex-col">
+        <div className="flex items-center justify-between border-b border-zinc-200 pt-8 pb-5">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="px-4 py-2 rounded-lg text-sm font-semibold shadow-sm text-zinc-700 border border-zinc-300 hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              form="settings"
+              className="px-4 py-2 bg-violet-600 text-white rounded-lg text-sm font-semibold shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form
+          id="settings"
+          className="mt-6 w-full flex flex-col gap-5 divide-y divide-zinc-200"
+        >
+          <div className="grid gap-3 grid-cols-form">
+            <label
+              htmlFor="firstName"
+              className="text-sm text-zinc-700 font-medium"
+            >
+              Name
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Control id="firstName" defaultValue="Oliva" />
+              </Input.Root>
+
+              <Input.Root>
+                <Input.Control defaultValue="Rhye" />
+              </Input.Root>
+            </div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="email"
+              className="text-sm text-zinc-700 font-medium"
+            >
+              Email address
+            </label>
+            <Input.Root>
+              <Input.Prefix>
+                <Mail className="w-5 h-5 text-zinc-500" />
+              </Input.Prefix>
+              <Input.Control id="email" defaultValue="af.rl@hotmail.com" />
+            </Input.Root>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="photo"
+              className="text-sm text-zinc-700 font-medium"
+            >
+              Your photo
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                This will be displayed on your profile.
+              </span>
+            </label>
+            <div className="flex items-start gap-5">
+              <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+                <User className="w-8 h-8 text-violet-500" />
+              </div>
+
+              <label
+                htmlFor="photo"
+                className="group flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
+              >
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
+                </div>
+
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="text-violet-700 font-semibold">
+                      Click to upload
+                    </span>{' '}
+                    or drag and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG, PNG, JPG or GIF (max. 800x400px)
+                  </span>
+                </div>
+              </label>
+
+              <input type="file" id="photo" className="sr-only" />
+            </div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="role" className="text-sm text-zinc-700 font-medium">
+              Role
+            </label>
+            <Input.Root>
+              <Input.Control id="role" defaultValue="Softawe Developer" />
+            </Input.Root>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm text-zinc-700 font-medium"
+            >
+              Country
+            </label>
+            <div>Select</div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm text-zinc-700 font-medium"
+            >
+              Timezone
+            </label>
+            <div>Select</div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="bio" className="text-sm text-zinc-700 font-medium">
+              Bio
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                Write a short introduction.
+              </span>
+            </label>
+            <div>Editor</div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="projects"
+              className="text-sm text-zinc-700 font-medium"
+            >
+              Portfolio projects
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                Share a few snippets of your work.
+              </span>
+            </label>
+            <div>Editor</div>
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <button
+              type="button"
+              className="px-4 py-2 rounded-lg text-sm font-semibold shadow-sm text-zinc-700 border border-zinc-300 hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-violet-600 text-white rounded-lg text-sm font-semibold shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
 }
---
+++ src/components/SettingsTabs/index.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import * as Tabs from '@radix-ui/react-tabs'
+import { TabItem } from './TabItem'
+import { useState } from 'react'
+
+export function SettingsTabs() {
+  const [currentTab, setCurrentTab] = useState('my-profile')
+
+  return (
+    <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
+      <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
+        <TabItem
+          title="My details"
+          value="my-details"
+          isSelected={currentTab === 'my-details'}
+        />
+
+        <TabItem
+          title="Profile"
+          value="profile"
+          isSelected={currentTab === 'profile'}
+        />
+
+        <TabItem
+          title="Password"
+          value="password"
+          isSelected={currentTab === 'password'}
+        />
+
+        <TabItem title="Team" value="team" isSelected={currentTab === 'team'} />
+
+        <TabItem title="Plan" value="plan" isSelected={currentTab === 'plan'} />
+
+        <TabItem
+          title="Billing"
+          value="billing"
+          isSelected={currentTab === 'billing'}
+        />
+
+        <TabItem
+          title="Email"
+          value="email"
+          isSelected={currentTab === 'email'}
+        />
+
+        <TabItem
+          title="Notification"
+          value="notifications"
+          isSelected={currentTab === 'notifications'}
+        />
+
+        <TabItem
+          title="Integrations"
+          value="integrations"
+          isSelected={currentTab === 'integrations'}
+        />
+
+        <TabItem title="API" value="api" isSelected={currentTab === 'api'} />
+      </Tabs.List>
+    </Tabs.Root>
+  )
+}
