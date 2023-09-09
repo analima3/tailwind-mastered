dc8441eacbe5d74e169628ce02967b35c4adb677
Ana Lima <af.rl@hotmail.com>
2023-09-09 15:28:33 -0300

    fix: element focus and add auto animate in the list

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,21 @@ const config: Config = {
           25: '#fcfaff',
         },
       },
+      keyframes: {
+        slideUpAndFade: {
+          from: {
+            opacity: '0',
+            transform: 'translateY(-2px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        slideUpAndFade: 'slideUpAndFade 0.6s cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
